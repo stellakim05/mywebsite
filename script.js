1f059e0d746e023df0b5501bc0ba62602a701fcb
byeolhakim <130078625+stellakim05@users.noreply.github.com>
2024-07-22 17:49:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    console.log('Form is attempting to submit.');
     alert('Form submitted!');
 });
+
